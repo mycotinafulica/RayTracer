23bf6a70cc0dbc0ac80af774a2a982b8bcb90eb5
mycotina <a.williamrusli@gmail.com>
2022-06-05 19:59:18 +0700

    Implement more ppm functionalities

--- src/main/java/org/aicl/raytracerchallenge/display/Canvas.java
+++ src/main/java/org/aicl/raytracerchallenge/display/Canvas.java
@@ -2,6 +2,10 @@ package org.aicl.raytracerchallenge.display;
 
 import org.aicl.raytracerchallenge.primitives.Color;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,6 +45,58 @@ public class Canvas {
             }
         }
         return result;
+    }
+
+    public void writeToPpmFile(String fileName) throws IOException {
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+        //header
+        writer.println("P3");
+        writer.println(width + " " + height);
+        writer.println("255");
+
+
+        for(int row = 0 ; row < height; row++){
+            StringBuilder line = new StringBuilder();
+            for(int col = 0; col < width; col++){
+                Color color = pixelAt(col, row);
+                int clampedRed  = clampValue((int)Math.round(color.red() * 255.0), 0, 255);
+                int clampedGreen = clampValue((int)Math.round(color.green() * 255.0), 0, 255);
+                int clampedBlue  = clampValue((int)Math.round(color.blue() * 255.0), 0, 255);
+
+                String[] colorStr = new String[]{
+                        Integer.toString(clampedRed),
+                        Integer.toString(clampedGreen),
+                        Integer.toString(clampedBlue)
+                };
+
+                for(int i = 0 ; i < colorStr.length ; i++){
+                    //if the current line plus the color string plus space larger than 70 (max char per line for ppm)
+                    //To be safe we use equality (in case the new line is counted as character)
+                    if((line.length() + colorStr[i].length() + 1) >= 70){
+                        //flush
+                        writer.println(line.toString());
+                        line = new StringBuilder();
+                    }
+
+                    if(line.length() == 0){
+                        line.append(colorStr[i]);
+                    }
+                    else{
+                        line.append(" ");
+                        line.append(colorStr[i]);
+                    }
+                }
+            }
+            writer.println(line);
+        }
+
+        writer.close();
+    }
+
+    private int clampValue(int value, int min, int max){
+        if(value < min)
+            return min;
+        return Math.min(value, max);
     }
 
     public int getWidth() {
