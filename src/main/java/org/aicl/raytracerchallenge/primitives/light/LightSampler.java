bcc7e5c338eef87d3a5edc2a698f9e6cdee037c3
mycotina <a.williamrusli@gmail.com>
2022-06-23 21:45:03 +0700

    Implemented!!

---
+++ src/main/java/org/aicl/raytracerchallenge/primitives/light/LightSampler.java
@@ -0,0 +1,34 @@
+package org.aicl.raytracerchallenge.primitives.light;
+
+import org.aicl.raytracerchallenge.primitives.*;
+
+public class LightSampler {
+    public static Color lighting(Material m, PointLight light, Point point, Vector eyev, Vector normal){
+        Color effectiveColor = m.color.multiply(light.intensity);
+
+        Tuple lightv = light.position.subtract(point).normalize();
+        Color ambient = effectiveColor.multiply(m.ambient);
+
+        double lightDotNormal = TupleOperation.dot(lightv, normal);
+        Color diffuse;
+        Color specular;
+        if(lightDotNormal < 0){
+            diffuse = new Color(0, 0, 0);
+            specular = new Color(0, 0, 0);
+        }
+        else{
+            diffuse = effectiveColor.multiply(m.diffuse).multiply(lightDotNormal);
+            Tuple reflecv = TupleOperation.reflect(new Vector(lightv.negates()), normal);
+            double reflectDotEye = TupleOperation.dot(reflecv, eyev);
+            if(reflectDotEye < 0){
+                specular =  new Color(0, 0, 0);
+            }
+            else{
+                double factor = Math.pow(reflectDotEye, m.shininess);
+                specular = light.intensity.multiply(m.specular).multiply(factor);
+            }
+        }
+
+        return ambient.add(diffuse).add(specular);
+    }
+}
---
+++ src/test/java/org/aicl/raytracerchallenge/primitives/light/LightSamplerTest.java
@@ -0,0 +1,71 @@
+package org.aicl.raytracerchallenge.primitives.light;
+
+import org.aicl.raytracerchallenge.primitives.Color;
+import org.aicl.raytracerchallenge.primitives.Material;
+import org.aicl.raytracerchallenge.primitives.Point;
+import org.aicl.raytracerchallenge.primitives.Vector;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class LightSamplerTest {
+    private static Material m;
+    private static Point position;
+
+    @BeforeAll
+    static void init(){
+        m = new Material();
+        position = new Point(0, 0 ,0);
+    }
+
+    //illustrations on page 86 - 87
+    @Test
+    public void testEyeDirectlyBetweenLightAndSurface(){
+        Vector eyev = new Vector(0, 0, -1);
+        Vector normal = new Vector(0, 0, -1);
+        PointLight light = new PointLight(new Point(0, 0, -10), new Color(1, 1, 1));
+
+        Color result = LightSampler.lighting(m, light, position, eyev, normal);
+        assertTrue(result.isIdentical(new Color(1.9, 1.9, 1.9)));
+    }
+
+    @Test
+    public void testEyeRotated45Degree(){
+        Vector eyev = new Vector(0, Math.sqrt(2)/2.0, -Math.sqrt(2)/2.0);
+        Vector normal = new Vector(0, 0, -1);
+        PointLight light = new PointLight(new Point(0, 0, -10), new Color(1, 1, 1));
+
+        Color result = LightSampler.lighting(m, light, position, eyev, normal);
+        assertTrue(result.isIdentical(new Color(1.0, 1.0, 1.0)));
+    }
+
+    @Test
+    public void testLightRotated45Degree(){
+        Vector eyev = new Vector(0, 0, -1);
+        Vector normal = new Vector(0, 0, -1);
+        PointLight light = new PointLight(new Point(0, 10, -10), new Color(1, 1, 1));
+
+        Color result = LightSampler.lighting(m, light, position, eyev, normal);
+        assertTrue(result.isIdentical(new Color(0.7364, 0.7364, 0.7364)));
+    }
+
+    @Test
+    public void testEyeDirectlyOnReflection(){
+        Vector eyev = new Vector(0, -Math.sqrt(2)/2.0, -Math.sqrt(2)/2.0);
+        Vector normal = new Vector(0, 0, -1);
+        PointLight light = new PointLight(new Point(0, 10, -10), new Color(1, 1, 1));
+
+        Color result = LightSampler.lighting(m, light, position, eyev, normal);
+        assertTrue(result.isIdentical(new Color(1.6364, 1.6364, 1.6364)));
+    }
+
+    @Test
+    public void testLightBehindSurface(){
+        Vector eyev = new Vector(0, 0, -1);
+        Vector normal = new Vector(0, 0, -1);
+        PointLight light = new PointLight(new Point(0, 10, 10), new Color(1, 1, 1));
+
+        Color result = LightSampler.lighting(m, light, position, eyev, normal);
+        assertTrue(result.isIdentical(new Color(0.1, 0.1, 0.1)));
+    }
+}
