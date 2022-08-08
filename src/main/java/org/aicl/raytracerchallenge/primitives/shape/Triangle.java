0df7a0f1c519e3568556aa2634b5d2ccfbca02a3
mycotina <a.williamrusli@gmail.com>
2022-08-08 21:29:13 +0700

    Triangle Intersection

--- src/main/java/org/aicl/raytracerchallenge/primitives/shape/Triangle.java
+++ src/main/java/org/aicl/raytracerchallenge/primitives/shape/Triangle.java
@@ -1,12 +1,11 @@
 package org.aicl.raytracerchallenge.primitives.shape;
 
-import org.aicl.raytracerchallenge.primitives.Point;
-import org.aicl.raytracerchallenge.primitives.Tuple;
-import org.aicl.raytracerchallenge.primitives.TupleOperation;
-import org.aicl.raytracerchallenge.primitives.Vector;
+import org.aicl.raytracerchallenge.primitives.*;
+import org.aicl.raytracerchallenge.primitives.ray.Intersection;
 import org.aicl.raytracerchallenge.primitives.ray.Ray;
 import org.aicl.raytracerchallenge.primitives.ray.RayIntersection;
 
+import java.util.List;
 import java.util.UUID;
 
 public class Triangle extends Shape{
@@ -38,7 +37,24 @@ public class Triangle extends Shape{
 
     @Override
     public RayIntersection localIntersect(Ray transformedRay) {
-        return null;
+        Vector directionCrossE2 = TupleOperation.cross(transformedRay.direction, e2);
+        double det = TupleOperation.dot(e1, directionCrossE2);
+        if(Math.abs(det) <= Constant.epsilon)
+            return new RayIntersection(0, List.of());
+
+        double f = 1.0/det;
+        Tuple p1ToRayOrigin = transformedRay.origin.subtract(p1);
+        double u = f * TupleOperation.dot(p1ToRayOrigin, directionCrossE2);
+        if(u < 0 || u > 1)
+            return new RayIntersection(0, List.of());
+
+        Vector originCrossE1 = TupleOperation.cross(p1ToRayOrigin, e1);
+        double v = f* TupleOperation.dot(transformedRay.direction, originCrossE1);
+        if(v < 0 || (u + v) > 1)
+            return new RayIntersection(0, List.of());
+
+        double t = f* TupleOperation.dot(e2, originCrossE1);
+        return new RayIntersection(1, List.of(new Intersection( t, this)));
     }
 
     @Override
